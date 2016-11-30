5debf6a44bc25fd89f10ce18037dd85bf0d1e515
Sunil Joshi <joshisunilis@hotmail.com>
2016-11-30 15:02:12 +0530

    MyFirstComponent

---
+++ src/MyFirstComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyFirstComponent = React.createClass({
+  render() {
+    return (
+      <h1>Hello, world!</h1>
+    );
+  }
+});
+
+export default MyFirstComponent;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MyFirstComponent from './MyFirstComponent';
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <MyFirstComponent />,
   document.getElementById('app')
 );
