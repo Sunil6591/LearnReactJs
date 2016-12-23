ac98cf004649b7c8177e1076e50a1413b7f0c939
Sunil Joshi <joshisunilis@hotmail.com>
2016-12-23 13:35:21 +0530

    a step to build production level server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const compression = require('compression');
+const port = process.env.PORT || 8181
+const app = express()
+
+app.use(compression());
+// serve static assets normally
+app.use(express.static(__dirname + '/dist'))
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(port)
+
+console.log("server started on port " + port)
